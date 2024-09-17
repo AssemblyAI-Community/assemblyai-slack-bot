27759e6a5624997ed154375dafe9d6d6e7889955
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-09-17 19:44:27 -0400

    Add user agent

--- src/assemblyai-client.ts
+++ src/assemblyai-client.ts
@@ -1,8 +1,15 @@
+import packageJson from '../package.json';
 import { AssemblyAI } from "assemblyai";
 
 export function getAssemblyAIClient(): AssemblyAI {
   return new AssemblyAI({
     apiKey: process.env.ASSEMBLYAI_API_KEY!,
     baseUrl: process.env.ASSEMBLYAI_BASE_URL || undefined,
+    userAgent: {
+      integration: {
+        name: 'Slack bot',
+        version: packageJson.version
+      }
+    }
   });
 }
